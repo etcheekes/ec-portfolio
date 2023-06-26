1fff71f4d45a4dd0ba2d30b3642d17612f09a38d
etcheekes <echeekes@gmail.com>
2023-06-26 19:48:45 +0100

    refactored js code that reveals hidden elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,28 +3,43 @@ window.addEventListener('load', function() {
     console.log("Eleventy linked to js");
 });
 
-function revealProjectText(selectOptionsId, technicalExpId, innerWorkingsId){
+/* reveal text functions */
+
+// function to reveal hidden elements from select option
+function revealText(element, text, selectOption) {
+    if (selectOption.value === text) {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
+
+function revealProjectText(selectOptionsId, technicalExpId, innerWorkingsId) {
     console.log("reveal function linked")
     // reference select options and hidden information
     const selectOption = document.querySelector(selectOptionsId);
     const technicalExp = document.querySelector(technicalExpId);
     const innerWorkings = document.querySelector(innerWorkingsId);
 
-    // display information based on value of selectOption
+    // reveal text
     selectOption.addEventListener("change", () => {
-        if (selectOption.value === "technical-experience") {
-            technicalExp.style.display = "block";
-        } else {
-            technicalExp.style.display = "none";
-        } 
-        
-        if (selectOption.value === "inner-workings") {
-            innerWorkings.style.display = "block";
-        } else {
-            innerWorkings.style.display = "none";
-        }
+        revealText(technicalExp, "technical-experience", selectOption);
+        revealText(innerWorkings, "inner-workings", selectOption);
+    });
+};
 
-        
+function revealAboutText(selectOptionsId, programmingJourneyId, decisionToProgramId, educationId) {
+
+    // reference select options and hidden information
+    const selectOptionAbout = document.querySelector(selectOptionsId);
+    const programmingJourney = document.querySelector(programmingJourneyId);
+    const decisionToProgram = document.querySelector(decisionToProgramId);
+    const education = document.querySelector(educationId);
+
+    // reveal text
+    selectOptionAbout.addEventListener("change", () => {
+        revealText(programmingJourney, "programming-journey", selectOptionAbout);
+        revealText(decisionToProgram, "decision-to-program", selectOptionAbout);
+        revealText(education, "education", selectOptionAbout);
     })
-
 }
