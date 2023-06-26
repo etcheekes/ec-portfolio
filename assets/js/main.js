75db8780a6800d3e150db46a9c75f131caeb69bc
etcheekes <echeekes@gmail.com>
2023-06-26 13:01:51 +0100

    revealProjectText js function creation and implementation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,30 @@
 // Run this function when the whole page is loaded
 window.addEventListener('load', function() {
-    console.log(" Eleventy linked to js");
+    console.log("Eleventy linked to js");
 });
+
+function revealProjectText(selectOptionsId, technicalExpId, innerWorkingsId){
+    console.log("reveal function linked")
+    // reference select options and hidden information
+    const selectOption = document.querySelector(selectOptionsId);
+    const technicalExp = document.querySelector(technicalExpId);
+    const innerWorkings = document.querySelector(innerWorkingsId);
+
+    // display information based on value of selectOption
+    selectOption.addEventListener("change", () => {
+        if (selectOption.value === "technical-experience") {
+            technicalExp.style.display = "block";
+        } else {
+            technicalExp.style.display = "none";
+        } 
+        
+        if (selectOption.value === "inner-workings") {
+            innerWorkings.style.display = "block";
+        } else {
+            innerWorkings.style.display = "none";
+        }
+
+        
+    })
+
+}
